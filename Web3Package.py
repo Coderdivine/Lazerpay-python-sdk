51ff09947be3e35ec4cc151a75af85f12c84f20e
Coderdivine <chimdi4332@gmail.com>
2022-07-16 09:54:12 +0100

    added new web3 class

--- Web3Package.py
+++ Web3Package.py
@@ -3,3 +3,17 @@ web3 = Web3(Web3.HTTPProvider("https://bsc-dataseed4.binance.org"))
 print(web3.isConnected())
 #blocks = web3.eth.get_block('latest')
 #print(blocks)
+class WebProvider():
+    def __init__(self):
+        pass
+    def BlockNumber(self):
+       block =  web3.eth.blockNumber()
+       return block
+    def getBalance(self,address,type): 
+        balance  = web3.eth.getBalance(address)
+        if type == 'from':
+            return web3.fromWei(balance, 'ether')
+        elif type == 'to':
+            return web3.toWei(balance,'ether')
+        else:
+            return balance
