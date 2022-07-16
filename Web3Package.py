2d4ac94c7188821c1acf9e8cc163006d86ee6218
Coderdivine <chimdi4332@gmail.com>
2022-07-16 09:32:31 +0100

    update

--- Web3Package.py
+++ Web3Package.py
@@ -1,5 +1,5 @@
-from web3 import Web3, EthereumTesterProvider
-w3 = Web3(EthereumTesterProvider())
+from web3 import Web3
+w3 = Web3()
 w3.isConnected()
 blocks = w3.eth.get_block('latest')
 print(blocks)
