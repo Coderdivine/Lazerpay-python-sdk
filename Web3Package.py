46e06072fa412a226054f7fcc4207786b4e7cea6
Coderdivine <chimdi4332@gmail.com>
2022-07-16 09:45:28 +0100

    console.log(isConnected)

--- Web3Package.py
+++ Web3Package.py
@@ -1,5 +1,5 @@
 from web3 import Web3
 web3 = Web3(Web3.HTTPProvider("https://bsc-dataseed4.binance.org"))
-web3.isConnected()
+print(web3.isConnected())
 #blocks = web3.eth.get_block('latest')
 #print(blocks)
