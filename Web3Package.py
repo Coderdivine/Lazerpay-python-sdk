7b953b76e95828cb63776a77e22e552316b0c9b1
Coderdivine <chimdi4332@gmail.com>
2022-07-16 09:37:58 +0100

    commits

--- Web3Package.py
+++ Web3Package.py
@@ -1,5 +1,5 @@
 from web3 import Web3
-w3 = Web3()
-w3.isConnected('https://bsc-dataseed4.binance.org')
+w3 = Web3('https://bsc-dataseed4.binance.org')
+#w3.isConnected()
 blocks = w3.eth.get_block('latest')
 print(blocks)
