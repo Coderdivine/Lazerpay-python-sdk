a765f3c3a63d4f91091a058ed97ff9b234dd60a4
Coderdivine <chimdi4332@gmail.com>
2022-07-16 10:02:12 +0100

    Done with web3 class function

--- Web3Package.py
+++ Web3Package.py
@@ -1,8 +1,7 @@
 from web3 import Web3
 web3 = Web3(Web3.HTTPProvider("https://bsc-dataseed4.binance.org"))
 print(web3.isConnected())
-#blocks = web3.eth.get_block('latest')
-#print(blocks)
+#no params
 class WebProvider():
     def __init__(self):
         pass
